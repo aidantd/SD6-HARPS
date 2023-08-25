7ff6c9bbfb137d38ddb81fd8375a6883a727a6e9
aidnatd <aidantdion@gmail.com>
2023-08-25 10:54:20 -0400

    Final basic commit, create project structure

--- main/main.c
+++ main/main.c
@@ -1,15 +1,13 @@
+// Includes
 #include <stdio.h>
 
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 
-void hello_task(void *pvParameter) {
-    while (1) {
-        printf("Hello world!\n");
-        printf("This is the start of the HARPS project.\n\n");
-        vTaskDelay(1000 / portTICK_PERIOD_MS);
-    }
-}
+// External Dependencies
+extern void hello_task(void *pvParameter);
+
+// Declarations
 
 void app_main(void) {
     xTaskCreate(&hello_task, "hello_task", 2048, NULL, 5, NULL);
