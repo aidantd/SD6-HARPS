46193fa09792e23c537e0af12a586cd9a5597137
aidnatd <aidantdion@gmail.com>
2023-09-24 17:37:51 -0400

    Cleanup conversoins for maintainablility

--- main/peripherals/sen0170.c
+++ main/peripherals/sen0170.c
@@ -5,13 +5,18 @@
 
 // Declarations
 #define ADC_SEN0170 ADC1_CHANNEL_0
+#define ADC_VOLTAGE_TO_METERS_PER_SECOND 6
 
-// Calculates the wind speed based off the formula provided by the manufacturer
+// Performing the calculation with a conversion values allows us to avoid floating point math.
+#define ADC_METERS_PER_SECOND_TO_MILES_PER_HOUR(value) ((value * 223694) / MILES_PER_HOUR_CONVERSION_VALUE)
+#define MILES_PER_HOUR_CONVERSION_VALUE 100000
+
+// Returns the wind speed in mi/hr as a 32-bit unsigned integer.
 static uint32_t calculateWindSpeed(uint32_t readVoltage) {
-    return readVoltage * 6;
+    return ADC_METERS_PER_SECOND_TO_MILES_PER_HOUR(readVoltage * ADC_VOLTAGE_TO_METERS_PER_SECOND);
 }
 
-// Reads in the recorded voltage from the anemometer and converts it to a wind speed
+// Returns the read wind speed in mi/hr as a 32-bit unsigned integer.
 uint32_t readSen0170(void) {
     uint32_t readVoltage = 0;
     readVoltage = readADC(ADC_SEN0170);
